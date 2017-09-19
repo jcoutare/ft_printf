73d60bf3613adefe9e2617bcea0ea7bc93fa149b
Julien Coutarel <jcoutare@student.42.fr>
2017-09-19 13:51:34 +0200

    7974564 normeerror

--- srcs/itoa_base.c
+++ srcs/itoa_base.c
@@ -6,34 +6,34 @@
 /*   By: jcoutare <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/05 14:25:04 by jcoutare          #+#    #+#             */
-/*   Updated: 2017/09/18 13:30:42 by jcoutare         ###   ########.fr       */
+/*   Updated: 2017/09/19 13:49:13 by jcoutare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/lol.h"
 
-char *ft_itoa_base(unsigned long long nbr, unsigned int base)
+char	*ft_itoa_base(unsigned long long nbr, unsigned int base)
 {
-  int tab[16] = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
-  char *str;
-  int len;
-  unsigned long long nb;
+	int tab[16] = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+	char *str;
+	int len;
+	unsigned long long nb;
 
-  nb = nbr;
-  len = 1;
-  while (nb >= base)
-    {
-      nb = nb / base;
-      len++;
-    }
-  if ((str = malloc(sizeof(char) * (len + 1))) == NULL)
-    return (NULL);
-  str[len--] = 0;
-  while (nbr >= base)
-    {
-      str[len--] = tab[nbr % base];
-      nbr = nbr / base;
-    }
-  str[len] = tab[nbr % base];
-  return(str);
+	nb = nbr;
+	len = 1;
+	while (nb >= base)
+	{
+		nb = nb / base;
+		len++;
+	}
+	if ((str = malloc(sizeof(char) * (len + 1))) == NULL)
+		return (NULL);
+	str[len--] = 0;
+	while (nbr >= base)
+	{
+		str[len--] = tab[nbr % base];
+		nbr = nbr / base;
+	}
+	str[len] = tab[nbr % base];
+	return (str);
 }
